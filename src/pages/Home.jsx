1b02e4de01178e6788853f047da3fce666db0730
Josh Garvin <84492192+joshgarv1n@users.noreply.github.com>
2024-10-14 18:27:03 -0500

    Re-design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,18 +3,18 @@ import Footer from "../components/Footer";
 
 const Home = () => {
     return (
-        <div className="bg-[url('./assets/mountains-bg.jpg')] h-screen bg-cover bg-center">
-            <div className="flex justify-center items-center flex-col pt-36">
-                <div className="flex flex-col items-start transform -translate-x-48">
-                    <h1 className="font-marker text-8xl">Josh Garvin</h1>
-                    <h2 className= "font-marker text-5xl">Software Engineer</h2>
-                </div>
-                <div className="flex justify-center items-center pt-60">
-                    <FaExclamationTriangle className="mr-4" size={30} />
-                    <h2 className= "font-cinzel text-5xl">Under Construction</h2>
-                    <FaExclamationTriangle className="ml-4" size={30} />
-                </div>
+        <div className="bg-[url('./assets/mountains-bg.jpg')] h-screen bg-cover bg-center flex flex-col">
+            <div className="flex flex-col items-center pt-20 sm:pt-5">
+                <h1 className="font-marker text-5xl md:text-8xl">Josh Garvin</h1>
+                <h2 className= "font-marker text-3xl md:text-5xl">Software Engineer</h2>
             </div>
+            <div className="flex flex-1 justify-center items-start py-24">
+                <div className="flex items-center">
+                    <FaExclamationTriangle className="mr-2 md:mr-4" size={25} />
+                    <h2 className= "font-cinzel text-xl md:text-3xl">Under Construction</h2>
+                    <FaExclamationTriangle className="ml-2 md:ml-4" size={25} />
+                </div>
+            </div> 
             <Footer />
         </div>
     );
